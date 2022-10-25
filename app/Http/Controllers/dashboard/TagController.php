e0ae549d4538277cf4f3f8de5a748c9e9973e41e
Efthakhar <efthakharbin@gmail.com>
2022-10-25 22:26:00 +0600

    tag store method added to controller

--- app/Http/Controllers/dashboard/TagController.php
+++ app/Http/Controllers/dashboard/TagController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\dashboard;
 
 use App\Http\Controllers\Controller;
+use App\Models\Tag;
 use Illuminate\Http\Request;
 
 class TagController extends Controller
@@ -10,5 +11,28 @@ class TagController extends Controller
     function create()
     {           
         return  view('dashboard.tag.create');
+    }
+
+    function store(Request $request)
+    {
+        
+        $validatedData = $request->validate([
+          'tag_name' => 'required|unique:tags|max:25',
+          'tag_slug' => 'unique:tags|max:25',      
+        ]);
+        
+        $slug = $request->tag_slug ? 
+        strtolower(str_replace(' ','-',$request->tag_slug))
+        :strtolower(str_replace(' ','-',$request->tag_name));
+
+        $tag = new Tag();
+        $tag->tag_name = $request->tag_name;
+        $tag->tag_slug = $slug;
+        $tag->category_description = $request->tag_description;
+
+        $tag->save();
+        return redirect('/dashboard/tags/create');
+             
+
     }
 }
